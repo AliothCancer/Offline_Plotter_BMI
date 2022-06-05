39f6f047cff3d98137d3bc9cc97ca229f9678812
AliothCancer <ortjuly7@gmail.com>
2022-06-05 19:07:42 +0200

    deleting offline_plotter.ui

--- offline_plotter_threads.py
+++ offline_plotter_threads.py
@@ -2,17 +2,20 @@ import pyqtgraph as pg
 from PyQt5.QtGui import QPen, QColor
 from PyQt5 import QtCore, QtWidgets, uic
 from PyQt5.QtWidgets import QFileDialog
-import sys, time, math, os
+import sys, time
 from loaded_file_parsing import get_columns, get_data
 
 
-#QtWidgets.QCheckBox.checkState()
+#QtWidgets.QComboBox.setItemText()
 
 class OfflinePlotter(QtWidgets.QMainWindow):
     def __init__(self):
         QtWidgets.QMainWindow.__init__(self)
         self.ui = uic.loadUi("offline_plotter_bmi.ui", self)
- 
+
+        self.setWindowTitle("BMI Offline Plotter")
+
+
         # THREADS CONTAINER
         #https://www.youtube.com/watch?v=k5tIk7w50L4&t=4s
         self.thread={}
@@ -45,19 +48,28 @@ class OfflinePlotter(QtWidgets.QMainWindow):
             self.checkBox_8
         ]
 
+        self.checkBox_9.stateChanged.connect(lambda: self.show_all_signals(self.checkBox_9))
+
         self.colors = [
-            (224, 27, 36),
+            (224, 27, 36),  # signal1
             (26, 95, 180),
             (38, 162, 105),
             (246, 211, 45),
             (255, 120, 0),
             (145, 65, 172),
             (14, 255, 0),
-            (71, 195, 167)
+            (71, 195, 167)  # signal8
         ]
 
 
     # NORMAL FUNCTIONS
+    def show_all_signals(self, QCheckBox):
+        for i in self.checkBoxes:
+            if i.isChecked() == False:
+                i.setChecked(True)
+            else:
+                i.setChecked(False)
+
     def load_file(self):
         print(type(self))
         options = QFileDialog.Options()
@@ -68,22 +80,26 @@ class OfflinePlotter(QtWidgets.QMainWindow):
 
         columns = get_columns(self.file_name)
 
-        for n, i in enumerate(columns):
-                self.comboBoxes[n].addItems(columns)
-                self.comboBoxes[n].itemText(n)
+        columns_num = len(columns)
+        for n in range(columns_num):
+            for i in range(columns_num):
+                self.comboBoxes[n].addItem(columns[i])
 
+            self.comboBoxes[n].itemText(n)
 
         self.data_2 = get_data(self.file_name, columns[1])
 
 
     def plot(self):
         self.graphicsView.clear()
-        for n in range(len(self.comboBoxes)):
-            if self.checkBoxes[n].checkState():
 
+        for n in range(len(self.comboBoxes)):
+            print(self.comboBoxes[n].currentText, self.comboBoxes[n].currentIndex())
+            if self.checkBoxes[n].checkState():
                 signal = self.comboBoxes[n].currentText()
                 print(signal)
                 self.data = get_data(self.file_name, signal)
+
                 y = []
 
                 for i in self.data:
